6a66f605d556963a34b30437bd038adddb1d805c
George Secure <geosebi2004@gmail.com>
2025-11-15 12:39:36 +0200

    Project setup

---
+++ frontend-web/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="@pulse-map/frontend-web" />
+    </div>
+  );
+}
+
+export default App;
